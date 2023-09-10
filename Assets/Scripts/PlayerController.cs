ac43581e51b9b5d28bd720d3f85d9f64cb503dcc
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-09-10 20:21:41 -0300

    Basic jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody m_playerRigidBody;
     [SerializeField] private GameObject m_camera;
     [SerializeField] private int m_speed;
-    [SerializeField] private GameObject m_yRotation;
-    [SerializeField] private GameObject m_zRotation;
     [SerializeField] private GameObject m_lookAt;
     [SerializeField] private int m_rotateSpeed;
+    [SerializeField] private float m_jumpSpeed;
     private bool m_isForwardDown;
     private bool m_isBackDown;
     private bool m_isRightDown;
@@ -41,28 +40,32 @@ public class PlayerController : MonoBehaviour
         m_isBackDown = Input.GetKey("s");
         m_isRightDown = Input.GetKey("d");
         m_isLeftDown = Input.GetKey("a");
-        m_isJumpDown = Input.GetKey("space");
+        m_isJumpDown = Input.GetKeyDown("space");
     }
     void Movement()
     {
         Vector3 inputVector = Vector3.zero;
-        //this.transform.rotation = m_camera.transform.rotation;
 
         float forwardMovement = Convert.ToInt32(m_isForwardDown) - Convert.ToInt32(m_isBackDown);
         float sideMovement = Convert.ToInt32(m_isRightDown) - Convert.ToInt32(m_isLeftDown);
-        
+
         inputVector += new Vector3(this.transform.forward.x, 0, this.transform.forward.z).normalized
                        * forwardMovement;
         inputVector += new Vector3(this.transform.right.x, 0, this.transform.right.z).normalized
                        * sideMovement;
+
         inputVector = Vector3.Normalize(inputVector);
 
         m_playerRigidBody.AddForce(inputVector * (m_speed * Time.deltaTime));
+        
     }
 
     void Jumping()
     {
-        
+        float jumping = Convert.ToInt32((m_isJumpDown));
+        Vector3 jumpVector = new Vector3(0, 1, 0);
+        //m_playerRigidBody.transform.position += new Vector3(0, jumping * m_jumpSpeed, 0);
+        m_playerRigidBody.AddForce(jumpVector * (jumping * m_jumpSpeed));
     }
 
     void Rotation()
