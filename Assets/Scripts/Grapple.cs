d6cde3bcd0043078321a814adeeca6a14fc6164a
TofuSynth <79347573+TofuSynth@users.noreply.github.com>
2023-10-01 18:09:30 -0300

    Center grapple extension on the camera, return grapple to starting position

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -8,14 +8,18 @@ public class Grapple : MonoBehaviour
 
     [SerializeField] private GameObject m_player;
     [SerializeField] private Rigidbody m_grapple;
+    [SerializeField] private GameObject m_grappleDirection;
     [SerializeField] private int m_grappleSpeed;
     private Vector3 RestingPosition;
     private bool m_isGrapplePressed;
+    private bool m_isGrappleLifted;
+    private bool m_isGrappleExtended = false;
     private SphereCollider m_grappleCollider;
 
     void ButtonCheck()
     {
         m_isGrapplePressed = Input.GetMouseButton(0);
+        m_isGrappleLifted = Input.GetMouseButtonUp(0);
     }
     private void Start()
     {
@@ -32,22 +36,43 @@ public class Grapple : MonoBehaviour
 
     void FireGrapple()
     {
-        if (m_isGrapplePressed)
+        if (m_isGrapplePressed && !m_isGrappleExtended)
         {
+            Vector3 grappleTarget = m_grappleDirection.transform.position;
+            float grappleSpeed = m_grappleSpeed * Time.deltaTime;
             transform.parent = null;
             m_grappleCollider.enabled = true;
-            m_grapple.AddForce(m_player.transform.forward * (m_grappleSpeed * Time.deltaTime));
+            m_grapple.position = Vector3.MoveTowards(m_grapple.position, grappleTarget, grappleSpeed);
+            if (Vector3.Distance(m_grapple.position, grappleTarget) < 0.001f)
+            {
+                m_isGrappleExtended = true;
+                ReturnGrapple();
+            }
         }
+        else if (m_isGrappleLifted)
+        {
+            m_isGrappleExtended = false;
+        }
+        else
+        {
+            ReturnGrapple();
+        }
+        
     }
 
     void RetractGrapple()
     {
         if (!m_isGrapplePressed && this.transform.position != RestingPosition)
         {
-            this.transform.parent = m_player.transform;
-            this.transform.localPosition = RestingPosition;
-            m_grappleCollider.enabled = false;
-            m_grapple.velocity = Vector3.zero;
+            ReturnGrapple();
         }
+    }
+
+    void ReturnGrapple()
+    {
+        this.transform.parent = m_player.transform;
+        this.transform.localPosition = RestingPosition;
+        m_grappleCollider.enabled = false;
+        m_grapple.velocity = Vector3.zero;
     }
 }
